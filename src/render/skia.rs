e59cddae20dfa43aaf7d8174a102d53b15ba67bd
JCLee777 <huanandli@icloud.com>
2024-11-06 17:54:12 +0800

    Update font rendering to use fontdb and support Chinese characters

--- src/render/skia.rs
+++ src/render/skia.rs
@@ -1,3 +1,4 @@
+use std::fs::read;
 use tiny_skia as sk;
 const SCALE: f32 = 3.0;
 
@@ -9,36 +10,52 @@ pub fn skia_render() {
     // fill page
     canvas.fill(sk::Color::WHITE);
 
-    let font = include_bytes!("../../assets/PingFang.ttc") as &[u8];
-    let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
-    let (metrics, bitmap) = font.rasterize('A', 16.0 * SCALE);
+    let mut db = fontdb::Database::new();
+    db.load_system_fonts();
+    const FAMILY_NAME: &str = "PingFang SC";
+    let query = fontdb::Query {
+        families: &[fontdb::Family::Name(FAMILY_NAME), fontdb::Family::SansSerif],
+        weight: fontdb::Weight::BOLD,
+        ..fontdb::Query::default()
+    };
 
-    println!("{:?}", metrics);
+    let id = db.query(&query).unwrap();
+    let (src, index) = db.face_source(id).unwrap();
 
-    let mut text_pixmap = sk::Pixmap::new(metrics.width as u32, metrics.height as u32).unwrap();
-    for x in 0..metrics.width {
-        for y in 0..metrics.height {
-            let pixel = bitmap[(x + y * metrics.width) as usize];
-            if pixel > 0 {
-                text_pixmap.pixels_mut()[y as usize * metrics.width as usize + x as usize] = sk::ColorU8::from_rgba(
+    if let fontdb::Source::File(ref path) = &src {
+        match read(path) {
+            Ok(font) => {
+                let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
+                let (metrics, bitmap) = font.rasterize('李', 18.0 * SCALE);
+                println!("{:?}", metrics);
+                let mut text_pixmap = sk::Pixmap::new(metrics.width as u32, metrics.height as u32).unwrap();
+                for x in 0..metrics.width {
+                    for y in 0..metrics.height {
+                        let pixel = bitmap[(x + y * metrics.width) as usize];
+                        if pixel > 0 {
+                            text_pixmap.pixels_mut()[y as usize * metrics.width as usize + x as usize] = sk::ColorU8::from_rgba(
+                                0,
+                                0,
+                                0,
+                                pixel
+                            ).premultiply();
+                        }
+                    }
+                }
+                canvas.draw_pixmap(
                     0,
                     0,
-                    0,
-                    pixel
-                ).premultiply();
+                    text_pixmap.as_ref(),
+                    &sk::PixmapPaint::default(),
+                    sk::Transform::identity(),
+                    None
+                );
+                // save to file
+                canvas.save_png("./target/test.png").unwrap();
+            }
+            Err(e) => {
+                eprintln!("Failed to read font file: {}", e);
             }
         }
     }
-
-    canvas.draw_pixmap(
-        0,
-        0,
-        text_pixmap.as_ref(),
-        &sk::PixmapPaint::default(),
-        sk::Transform::identity(),
-        None
-    );
-
-    // save to file
-    canvas.save_png("./target/test.png").unwrap();
 }
